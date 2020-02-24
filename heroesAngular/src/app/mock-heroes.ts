620da38538f2cfb2e0ddb718b9fdd553eba705d4
EdgarFernandez-141299 <edgarfernavila@gmail.com>
2020-02-24 14:10:17 -0600

    First commit

---
+++ heroesAngular/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+export const HEROES: Hero[] = [
+{ id: 1, name: 'Batman' },
+{ id: 2, name: 'Superman' },
+{ id: 3, name: 'Escorpion Dorado' },
+{ id: 4, name: 'Robin Hood' },
+{ id: 5, name: 'Linterna Verda' },
+{ id: 6, name: 'Edgar Man' },
+{ id: 7, name: 'Angelica Woman' },
+{ id: 8, name: 'Mujer Maravilla' },
+{ id: 9, name: 'Volcan De fuego' },
+{ id: 10, name: 'Duende Verde' }
+];
